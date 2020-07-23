0b8fde4b093bc1a3397438899239fc63ab1ed32a
Temitope Ajayi <na_me_men@yahoo.com>
2020-07-23 03:53:39 -0500

    Modified files

--- runfile.py
+++ runfile.py
@@ -3,4 +3,5 @@ from transport import FileParser
 file = 'input.txt'
 input_file = FileParser.FileParser(file)
 
+print(input_file.velocity)
 input_file.read_file()
--- transport/FileParser.py
+++ transport/FileParser.py
@@ -1,15 +1,61 @@
 class FileParser(object):
     def __init__(self, file):
         self.file = file
+        self.line = self.read_file()
 
     def read_file(self):
         with open(self.file) as file:
             line = file.readlines()
-        m = self.split_line(line[1])
-        print(type(m[1]))
+        return line
+        m = self.get_value(line[4])
+        print(m)
+
 
     @staticmethod
-    def split_line(value):
-        split_value = value.split("(")
+    def split_line(value, sep):
+        split_value = value.split(sep)
         return split_value
+
+    def get_value(self, value):
+        initial_split = self.split_line(value, "(")[1]
+        last_split = self.split_line(initial_split, ")")[0]
+        return last_split
+
+    @property
+    def xLength(self):
+        full_value = self.get_value(self.line[1])
+        value = self.split_line(full_value, ",")[0]
+        return value
+
+    @property
+    def yLength(self):
+        full_value = self.get_value(self.line[1])
+        value = self.split_line(full_value, ",")[1]
+        return value
+
+    @property
+    def latticeType(self):
+        value = self.get_value(self.line[2])
+        return value
+
+    @property
+    def transportType(self):
+        value = self.get_value(self.line[3])
+        return value
+
+    @property
+    def velocity(self):
+        value = self.get_value(self.line[4])
+        return value
+
+    @property
+    def velocity(self):
+        value = self.get_value(self.line[4])
+        return value
+
+    @property
+    def diffusion_coefficient(self):
+        value = self.get_value(self.line[4])
+        return value
+
 
